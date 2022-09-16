5f8404f9ab1f88874e27c9ba8409805e9f01734d
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2022-09-16 13:26:52 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -122,6 +122,10 @@ def NBRaddTimeline(image):
 # S1 PREPROCESSING
 ###########################################
 
+#The Sentinel-1 helper files are adopted from Mullissa, A.; Vollrath, A.; Odongo-Braun, C.; Slagter, B.; Balling, J.; Gou, Y.; Gorelick, N.; Reiche, J. 
+#Sentinel-1 SAR Backscatter Analysis Ready Data Preparation in Google Earth Engine. Remote Sens. 2021, 13, 1954. https://doi.org/10.3390/rs13101954
+
+#The original codes are found in https://github.com/adugnag/gee_s1_ard
 #-------------------------//
 # convert linear to db and vice versa
 #-------------------------//
